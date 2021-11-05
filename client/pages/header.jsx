444bbc030c5896e176f2ce75213782ed8e4f9671
Derek-Cruz <Derekcruz94@gmail.com>
2021-11-05 13:20:59 -0700

    Working on my CSS. #1

--- client/pages/header.jsx
+++ client/pages/header.jsx
@@ -4,8 +4,8 @@ export default function Header(props) {
   return (
     <header>
       <div className="container d-flex justify-content-center">
-        <div className="fs-1">
-            hello hello
+        <div className="header-jsx-style">
+            hello
         </div>
       </div>
     </header>
--- client/pages/home.jsx
+++ client/pages/home.jsx
@@ -3,7 +3,26 @@ import React from 'react';
 export default function Home(props) {
   return (
     <>
-    <div>home.jsx</div>
+    <div className="container testing2">
+      <div className="row testing">
+        <div className="col p-0">
+          <h2 className="h2-home-jsx">My Plans</h2>
+          <div className="">
+            test
+
+          </div>
+        </div>
+      </div>
+      <div className="row testing">
+        <div className="col p-0">
+          <h2 className="h2-home-jsx">Available</h2>
+          <div className="">
+            test2
+
+          </div>
+        </div>
+      </div>
+    </div>
     </>
   );
 }

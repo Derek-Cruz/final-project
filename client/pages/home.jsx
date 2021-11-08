c25c8a47dcdf43bc316397f98364330477e7a865
Derek-Cruz <Derekcruz94@gmail.com>
2021-11-07 21:07:51 -0800

    Working on being able to post. #1

--- client/pages/home.jsx
+++ client/pages/home.jsx
@@ -2,27 +2,24 @@ import React from 'react';
 
 export default function Home(props) {
   return (
-    // <div className="container container-home-jsx">
-    //   <div className="row plans-home-jsx">
-    //     <div className="col p-0">
-    //       <h2 className="h2-plans-jsx">My Plans</h2>
-    //       <div className="testing">
-    //         <p>this is where my active plans go</p>
-    //       </div>
-    //     </div>
-    //   </div>
-    //   <div className="row avail-home-jsx">
-    //     <div className="col p-0">
-    //       <h2 className="h2-avail-jsx">Available</h2>
-    //       <div className="testing">
-    //         <p>this is where my friends who are available go</p>
-    //       </div>
-    //     </div>
-    //     <button className="button-home-jsx">+</button>
-    //   </div>
-    // </div>
-    <div className="container bg-danger">
-      <div className="post-status-test">help</div>
-  </div>
+    <div className="container container-home-jsx">
+      <div className="row plans-home-jsx">
+        <div className="col p-0">
+          <h2 className="h2-plans-jsx">My Plans</h2>
+          <div className="testing">
+            <p>this is where my active plans go</p>
+          </div>
+        </div>
+      </div>
+      <div className="row avail-home-jsx">
+        <div className="col p-0">
+          <h2 className="h2-avail-jsx">Available</h2>
+          <div className="testing">
+            <p>this is where my friends who are available go</p>
+          </div>
+        </div>
+        <button className="button-home-jsx">+</button>
+      </div>
+    </div>
   );
 }
--- client/pages/post-status.jsx
+++ client/pages/post-status.jsx
@@ -1,9 +1,61 @@
 import React from 'react';
 
-export default function PostStatus(props) {
-  // need to add hash routing but need advice from scott
-  return (
-    <>
-    </>
-  );
+export default class PostStatus extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      time: '',
+      description: ''
+    };
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.reset = this.reset.bind(this);
+  }
+
+  handleChange(event) {
+    const value = event.target.value;
+    const name = event.target.name;
+
+    this.setState({ [name]: value });
+  }
+
+  handleSubmit(event) {
+    // console.log('state:', this.state);
+    event.preventDefault();
+  }
+
+  render() {
+    return (
+      <div className="container">
+        <div className="post-status-test">
+          <form onSubmit={this.handleSubmit} onReset={this.reset}>
+            <div>
+              <label htmlFor="post-time">need icon</label>
+              <input
+                name="time"
+                type="time"
+                id="post-time"
+                value={this.state.value}
+                onChange={this.handleChange} />
+            </div>
+            <div>
+              <label htmlFor="post-description">need icon</label>
+              <input
+                name="description"
+                type="text"
+                id="post-description"
+                value={this.state.value}
+                onChange={this.handleChange} />
+            </div>
+            <div className="button1">
+              <button className="button-style">Post</button>
+            </div>
+            <div className="button2">
+              <button className="button-style">Cancel</button>
+            </div>
+          </form>
+        </div>
+      </div>
+    );
+  }
 }

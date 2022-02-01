f03c10f9687c857b8b157a8ff48d187d720e18f7
Derek-Cruz <Derekcruz94@gmail.com>
2022-01-31 19:34:47 -0800

    Work in progress! #8

--- client/pages/profile.jsx
+++ client/pages/profile.jsx
@@ -10,7 +10,18 @@ export default class Profile extends React.Component {
 
   render() {
     return (
-      <></>
+      <div className="container container-home-jsx">
+        <div className="row noti-row-style">
+          <div className="col p-0">
+            <h2 className="noti-h2-style">testing profile</h2>
+            <div className="noti-jsx-margin">
+              <div className="row">
+              <p>testing render</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     );
   }
 

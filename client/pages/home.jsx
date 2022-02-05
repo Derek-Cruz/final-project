f07e239f4260c25c50a02f027e74dfb0bb48117b
Derek-Cruz <Derekcruz94@gmail.com>
2022-02-04 18:16:12 -0800

    Fixing a issue with the headers before moving on with profile view. #8

--- client/pages/home.jsx
+++ client/pages/home.jsx
@@ -63,9 +63,9 @@ export default class Home extends React.Component {
   render() {
     return (
       <div className="container container-home-jsx">
+        <h2 className="h2-plans-jsx">MY PLANS</h2>
         <div className="row plans-home-jsx">
-          <div className="col p-0">
-            <h2 className="h2-plans-jsx">MY PLANS</h2>
+          <div className="col p-0 testing-new-header">
             <div className="home-jsx-margin">
               {
                 this.state.isLoading
@@ -81,9 +81,9 @@ export default class Home extends React.Component {
             </div>
           </div>
         </div>
+        <h2 className="h2-pending-jsx">PENDING</h2>
         <div className="row pending-home-jsx">
-          <div className="col p-0">
-            <h2 className="h2-pending-jsx">PENDING</h2>
+          <div className="col p-0 testing-new-header">
             <div className="home-jsx-margin ">
               {
                 this.state.isLoading
@@ -99,9 +99,9 @@ export default class Home extends React.Component {
             </div>
           </div>
         </div>
+        <h2 className="h2-avail-jsx">AVAILABLE</h2>
         <div className="row avail-home-jsx">
-          <div className="col p-0">
-            <h2 className="h2-avail-jsx">AVAILABLE</h2>
+          <div className="col p-0 testing-new-header">
             <div className="home-jsx-margin ">
               <div className="row margin-test">
                 {

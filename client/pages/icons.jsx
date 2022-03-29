15fe4687b566f24e81929a9b0e8b5059340540fd
Derek-Cruz <Derekcruz94@gmail.com>
2022-03-29 08:33:37 -0700

    Fixing mediaQ.

--- client/pages/icons.jsx
+++ client/pages/icons.jsx
@@ -5,7 +5,7 @@ export default function Icons(props) {
       <div className="icon-position d-flex justify-content-center">
         <a className="icon-style" href="#"><i className="fas fa-home icon-fas"></i><span className="test-span">Home</span></a>
         <a className="icon-style" href="#search-people"><i className="fas fa-search icon-fas"></i><span className="test-span">Search</span></a>
-      <a className="icon-style" href="#post-status"><i className="fa-solid fa-circle-plus icon-fas circle-display"></i><span className="test-span">Create</span></a>
+        <a className="icon-style circle-display" href="#post-status"><i className="fa-solid fa-circle-plus icon-fas"></i><span className="test-span">Create</span></a>
         <a className="icon-style" href="#notification"><i className="fas fa-bell icon-fas"></i><span className="test-span">Notification</span></a>
         <a className="icon-style" href="#profile"><i className="fas fa-user icon-fas"></i><span className="test-span">Profile</span></a>
       </div>
